6afb5f04ac154ccf8a97af61adb5cfe555f1eed1
Aditya K <adityak.10102005@gmail.com>
2026-02-27 20:54:12 +0530

    fix: fixed color in not found page

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const NotFoundPage: React.FC = () => {
     return (
-        <div style={{ padding: '100px', textAlign: 'center' }}>
+        <div style={{ padding: '100px', textAlign: 'center', color: 'white' }}>
             <h1>404 - Not Found</h1>
             <p>The page you are looking for does not exist.</p>
             <Link to="/">Go Home</Link>
